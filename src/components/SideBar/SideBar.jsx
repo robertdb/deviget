f1c4c0abe0b5235c81035aa1ed9d409e449f6638
Robert Diaz Bejar <robertdibejar@gmail.com>
2020-04-26 16:08:32 -0300

    feat: button added

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Tabs, Typography, Box } from "@material-ui/core";
+import { Tabs, Typography, Grid, Button } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = (theme) => ({
   root: {
     backgroundColor: theme.palette.background.paper,
-    display: "flex",
-    height: "90vh",
+    height: "100vh",
   },
   tabs: {
     width: "100%",
+    display: "flex",
+    height: "90vh",
+  },
+  buttonContainer: {
+    height: "10vh",
   },
 });
 
@@ -22,19 +26,24 @@ const SideBarBase = (props) => {
   }
 
   return (
-    <Box className={classes?.root}>
-      <Tabs
-        fullWidth
-        orientation="vertical"
-        variant="scrollable"
-        value={""}
-        onChange={() => {}}
-        aria-label="Vertical tabs example"
-        className={classes.tabs}
-      >
-        {texts}
-      </Tabs>
-    </Box>
+    <Grid container direction="column" spacing={0} className={classes?.root}>
+      <Grid item>
+        <Tabs
+          fullWidth
+          orientation="vertical"
+          variant="scrollable"
+          value={""}
+          onChange={() => {}}
+          aria-label="Vertical tabs example"
+          className={classes.tabs}
+        >
+          {texts}
+        </Tabs>
+      </Grid>
+      <Grid item className={classes?.buttonContainer}>
+        <Button fullWidth>Default</Button>
+      </Grid>
+    </Grid>
   );
 };
 
