dc8456b447ac55df85648255d49227a7ec60a1ea
Robert Diaz Bejar <robertdibejar@gmail.com>
2020-04-27 01:32:14 -0300

    feat: parse data

--- src/ducks/post/utils.js
+++ src/ducks/post/utils.js
@@ -1,4 +1,24 @@
+import moment from "moment";
+
 export const parseData = (data) => {
-  console.log("Hey I am the parser", data);
-  return data;
+  console.log(data);
+  return data.map((item) => {
+    const {
+      author,
+      id,
+      num_comments,
+      thumbnail,
+      created_utc,
+      title,
+    } = item.data;
+
+    return {
+      author,
+      id,
+      num_comments,
+      thumbnail,
+      title,
+      time: moment.unix(created_utc).local().startOf("minute").fromNow(),
+    };
+  });
 };
