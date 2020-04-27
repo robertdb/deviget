e807b1e57d3cb628aef11767c59fdcb83428e851
Robert Diaz Bejar <robertdibejar@gmail.com>
2020-04-27 12:16:32 -0300

    feat: adjust layout

--- src/layout/HomeLayout/HomeLayout.jsx
+++ src/layout/HomeLayout/HomeLayout.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Hidden } from "@material-ui/core";
 import { SideBar, RightSide } from "../../components";
 
 const HomeLayout = () => {
   return (
     <Grid container>
-      <Grid item xs={4}>
+      <Grid item xs={12} md={4}>
         <Typography variant="h6">Reddit Post</Typography>
         <SideBar />
       </Grid>
-      <Grid item xs={8}>
-        <RightSide />
-      </Grid>
+      <Hidden smDown>
+        <Grid item md={8}>
+          <RightSide />
+        </Grid>
+      </Hidden>
     </Grid>
   );
 };
